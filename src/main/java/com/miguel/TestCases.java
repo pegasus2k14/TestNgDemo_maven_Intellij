8fd41037103201fa69147324e2ce82e584526d11
Miguel Ramos <angel_ramos@siman.com>
2022-11-29 10:15:10 -0600

    Initial commit

---
+++ src/main/java/com/miguel/TestCases.java
@@ -0,0 +1,12 @@
+package com.miguel;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    void test1(){
+        System.out.println("Im inside test1() method");
+    }
+
+}
